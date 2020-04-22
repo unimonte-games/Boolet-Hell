ba1fdfade09fc39afb2f9008230a051d40738beb
Luiz Fernando <luiz_fernandogalucci@hotmail.com>
2020-04-22 20:14:38 -0300

    Jueguito

---
+++ Assets/soldier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class soldier : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<NavMeshAgent>().SetDestination(Player.transform.position);
+    }
+}
---
+++ Assets/turret.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turret : MonoBehaviour
+{
+
+    private GameObject target;
+    private bool targetlocked;
+
+    public GameObject TurretTopPart;
+
+
+    private void Update()
+    {
+        if (targetlocked)
+        {
+            TurretTopPart.transform.LookAt(target.transform);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            target = other.gameObject;
+            targetlocked = true;
+        }
+    }
+
+}
