f1a7ef8ae12c3927232d7f9a5bae14f1be12baed
Gabriel Moretzsohn <progecty2@gmail.com>
2020-05-13 23:01:44 -0300

    Boolet Hell 1.2.1

--- Assets/Scripts/bulletskeleton.cs
+++ Assets/Scripts/bulletskeleton.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bulletSoldier : MonoBehaviour
+public class bulletskeleton : MonoBehaviour
 {
     private Vector3 firingpoint;
 
